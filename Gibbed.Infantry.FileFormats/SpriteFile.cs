5bae13b7c9bc983cea13db57c5de86a224b706a1
Rick Gibbed <rick@gibbed.us>
2011-06-14 04:29:30 +0000

    Preliminary implementation of SpriteFile.

---
+++ Gibbed.Infantry.FileFormats/SpriteFile.cs
@@ -0,0 +1,222 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.IO;
+using Gibbed.Helpers;
+
+namespace Gibbed.Infantry.FileFormats
+{
+    public class SpriteFile
+    {
+        public string Category;
+        public string Description;
+
+        public ushort AnimationTime;
+        public ushort Width;
+        public ushort Height;
+        public ushort RowCount;
+        public ushort ColumnCount;
+        public ushort ShadowCount;
+        public ushort LightCount;
+        public ushort YSortAdjust;
+        public ushort BlitMode;
+        public ushort RowMeaning;
+        public ushort Unknown20;
+        public byte MaxSolidIndex;
+        public byte SortTransform;
+        public byte UserPaletteStart;
+        public byte UserPalette;
+
+        public uint[] Palette = new uint[256];
+        public byte[] UserData;
+
+        public Sprite.Frame[] Frames;
+
+        public void Deserialize(Stream input)
+        {
+            var version = input.ReadValueU16();
+            if (version < 2 || version > 5)
+            {
+                throw new FormatException("unsupported cfs version");
+            }
+
+            var header = new Sprite.Header();
+            
+            if (version >= 5)
+            {
+                header = input.ReadStructure<Sprite.Header>();
+            }
+            else if (version >= 4)
+            {
+                var oldHeader = input.ReadStructure<Sprite.OldHeader4>();
+                header.FrameCount = oldHeader.FrameCount;
+                header.AnimationTime = oldHeader.AnimationTime;
+                header.Width = oldHeader.Width;
+                header.Height = oldHeader.Height;
+                header.RowCount = oldHeader.RowCount;
+                header.ColumnCount = oldHeader.ColumnCount;
+                header.ShadowCount = oldHeader.ShadowCount;
+                header.LightCount = oldHeader.LightCount;
+                header.UserDataSize = oldHeader.UserDataSize;
+                header.CompressionFlags = oldHeader.CompressionFlags;
+                header.MaxSolidIndex = oldHeader.MaxSolidIndex;
+                header.DataSize = oldHeader.DataSize;
+                header.Category = oldHeader.Category;
+                header.BlitMode = oldHeader.BlitMode;
+                header.RowMeaning = oldHeader.RowMeaning;
+                header.YSortAdjust = oldHeader.YSortAdjust;
+                header.SortTransform = oldHeader.SortTransform;
+                header.UserPaletteStart = oldHeader.UserPaletteStart;
+                header.UserPalette = oldHeader.UserPalette;
+                header.Description = oldHeader.Description;
+            }
+            else if (version >= 3)
+            {
+                var oldHeader = input.ReadStructure<Sprite.OldHeader3>();
+                header.FrameCount = oldHeader.FrameCount;
+                header.AnimationTime = oldHeader.AnimationTime;
+                header.Width = oldHeader.Width;
+                header.Height = oldHeader.Height;
+                header.RowCount = oldHeader.RowCount;
+                header.ColumnCount = oldHeader.ColumnCount;
+                header.ShadowCount = oldHeader.ShadowCount;
+                header.LightCount = oldHeader.LightCount;
+                header.UserDataSize = oldHeader.UserDataSize;
+                header.CompressionFlags = oldHeader.CompressionFlags;
+                header.MaxSolidIndex = oldHeader.MaxSolidIndex;
+                header.DataSize = oldHeader.DataSize;
+                header.Category = oldHeader.Category;
+                header.BlitMode = oldHeader.BlitMode;
+                header.RowMeaning = oldHeader.RowMeaning;
+                header.YSortAdjust = oldHeader.YSortAdjust;
+                header.SortTransform = oldHeader.SortTransform;
+                header.UserPaletteStart = oldHeader.UserPaletteStart;
+                header.UserPalette = oldHeader.UserPalette;
+            }
+            else if (version >= 1)
+            {
+                var oldHeader = input.ReadStructure<Sprite.OldHeader2>();
+                header.FrameCount = oldHeader.FrameCount;
+                header.AnimationTime = oldHeader.AnimationTime;
+                header.Width = oldHeader.Width;
+                header.Height = oldHeader.Height;
+                header.RowCount = oldHeader.RowCount;
+                header.ColumnCount = oldHeader.ColumnCount;
+                header.ShadowCount = oldHeader.ShadowCount;
+                header.LightCount = oldHeader.LightCount;
+                header.UserDataSize = oldHeader.UserDataSize;
+                header.CompressionFlags = oldHeader.CompressionFlags;
+                header.MaxSolidIndex = oldHeader.MaxSolidIndex;
+                header.DataSize = oldHeader.DataSize;
+            }
+
+            this.AnimationTime = header.AnimationTime;
+            this.Width = header.Width;
+            this.Height = header.Height;
+            this.RowCount = header.RowCount;
+            this.ColumnCount = header.ColumnCount;
+            this.ShadowCount = header.ShadowCount;
+            this.LightCount = header.LightCount;
+            this.MaxSolidIndex = header.MaxSolidIndex;
+
+            for (int i = 0; i < this.Palette.Length; i++)
+            {
+                this.Palette[i] = input.ReadValueU32();
+            }
+
+            this.UserData = new byte[header.UserDataSize];
+            if (input.Read(this.UserData, 0, this.UserData.Length) != this.UserData.Length)
+            {
+                throw new FormatException();
+            }
+
+            var frameInfos = new Sprite.FrameInfo[header.FrameCount];
+            for (int i = 0; i < frameInfos.Length; i++)
+            {
+                frameInfos[i] = input.ReadStructure<Sprite.FrameInfo>();
+            }
+
+            var data = input.ReadToMemoryStream(header.DataSize);
+
+            if (header.CompressionFlags != 0)
+            {
+                throw new NotSupportedException();
+            }
+            else if (header.Unknown20 != 0)
+            {
+                throw new NotSupportedException();
+            }
+
+            this.Frames = new Sprite.Frame[header.FrameCount];
+            for (int i = 0; i < header.FrameCount; i++)
+            {
+                var info = frameInfos[i];
+                data.Seek(info.Offset, SeekOrigin.Begin);
+
+                var frame = this.Frames[i] = new Sprite.Frame();
+
+                frame.X = info.X;
+                frame.Y = info.Y;
+                frame.Width = info.Width;
+                frame.Height = info.Height;
+
+                var lengths = new int[info.Height];
+                var max = 0;
+                for (int y = 0; y < info.Height; y++)
+                {
+                    int length = data.ReadValueU8();
+                    if (length == 0xFF)
+                    {
+                        length = data.ReadValueU16();
+                    }
+                    lengths[y] = length;
+                    max = Math.Max(max, length);
+                }
+
+                frame.Pixels = new byte[info.Width * info.Height];
+                
+                var scanline = new byte[max];
+                for (int y = 0, offset = 0; y < info.Height; y++, offset = y * info.Width)
+                {
+                    var length = lengths[y];
+                    if (data.Read(scanline, 0, length) != length)
+                    {
+                        throw new FormatException();
+                    }
+
+                    for (int x = 0; x < length; )
+                    {
+                        offset += (scanline[x] >> 4) & 0xF;
+                        var literalCount = scanline[x] & 0xF;
+                        if (literalCount > 0)
+                        {
+                            Array.Copy(scanline, x + 1, frame.Pixels, offset, literalCount);
+                        }
+                        offset += literalCount;
+                        x += 1 + literalCount;
+                    }
+                }
+            }
+        }
+    }
+}
